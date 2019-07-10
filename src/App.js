6e1719e6f172f10c6aad66519b14394104dda875
Niklas Noldin <niklas.noldin@gmail.com>
2019-07-10 17:46:23 +0200

    make frontend interactive and add colors to data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import React, {
-  useEffect
-} from "react";
+import { InputField } from "./InputField";
+import React, { useEffect, useState } from "react";
 import mapboxgl from "mapbox-gl";
 import "./App.css";
 import "mapbox-gl/dist/mapbox-gl.css";
-import geoJson from "./prepareData/outputJsonFiles/20190501000552.json";
 
-const initializeMap = () => {
+const initializeMap = (day, timestamp) => {
   mapboxgl.accessToken =
     "pk.eyJ1IjoibmlrbGFzbm9sZGluIiwiYSI6ImNqaGh0NWxzZDF4cGczNnFvbnd0ZHJjbHEifQ.9yoH6H8i310Snle05XVYGA";
   const map = new mapboxgl.Map({
@@ -15,96 +13,79 @@ const initializeMap = () => {
     center: [2.17, 41.375],
     zoom: 12
   });
-  console.log(geoJson);
+
+  let baseUrl =
+    "https://raw.githubusercontent.com/niklasnoldin/niklasnoldin.github.io/master/output/";
+
   map.on("load", () => {
-    let timestamp = 20190501000552
+    let timestamp = 20190501000552;
     map.addSource("streets", {
-      "type": "geojson",
-      "data": require(`./prepareData/outputJsonFiles/${timestamp}.json`)
+      type: "geojson",
+      data: `${baseUrl}/${day}/${timestamp}.json`
     });
 
-    map.addLayer({
-      "id": "traffic-0",
-      "type": "line",
-      "source": "streets",
-      "paint": {
-        "line-color": "#fff",
-        "line-width": 5
-      },
-      "filter": ["==", "trafficIntensity", "0"]
+    let popup = new mapboxgl.Popup({
+      closeButton: false,
+      closeOnClick: false
     });
-    map.addLayer({
-      "id": "traffic-1",
-      "type": "line",
-      "source": "streets",
-      "paint": {
-        "line-color": "#fdd",
-        "line-width": 5
-      },
-      "filter": ["==", "trafficIntensity", "1"]
-    });
-    map.addLayer({
-      "id": "traffic-2",
-      "type": "line",
-      "source": "streets",
-      "paint": {
-        "line-color": "#faa",
-        "line-width": 5
-      },
-      "filter": ["==", "trafficIntensity", "2"]
-    });
-    map.addLayer({
-      "id": "traffic-3",
-      "type": "line",
-      "source": "streets",
-      "paint": {
-        "line-color": "#f88",
-        "line-width": 5
-      },
-      "filter": ["==", "trafficIntensity", "3"]
-    });
-    map.addLayer({
-      "id": "traffic-4",
-      "type": "line",
-      "source": "streets",
-      "paint": {
-        "line-color": "#f55",
-        "line-width": 5
-      },
-      "filter": ["==", "trafficIntensity", "4"]
-    });
-    map.addLayer({
-      "id": "traffic-5",
-      "type": "line",
-      "source": "streets",
-      "paint": {
-        "line-color": "#f33",
-        "line-width": 5
-      },
-      "filter": ["==", "trafficIntensity", "5"]
-    });
-    map.addLayer({
-      "id": "traffic-6",
-      "type": "line",
-      "source": "streets",
-      "paint": {
-        "line-color": "#f00",
-        "line-width": 5
-      },
-      "filter": ["==", "trafficIntensity", "6"]
-    })
-  })
+
+    const onMouseLeaveHandler = e => {
+      map.getCanvas().style.cursor = "";
+      popup.remove();
+    };
+
+    const onMouseEnterHandler = e => {
+      map.getCanvas().style.cursor = "pointer";
+
+      var coordinates = e.features[0].geometry.coordinates.slice();
+      var traffic = e.features[0].properties.trafficIntensity;
+
+      while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+        coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+      }
+
+      popup
+        .setLngLat(e.lngLat)
+        .setHTML(`Traffic: ${traffic}`)
+        .addTo(map);
+    };
+
+    for (let i = 0; i < 7; i++) {
+      map.addLayer({
+        id: `traffic-${i}`,
+        type: "line",
+        source: "streets",
+        paint: {
+          "line-color": `rgb(${Math.pow(255, 1 / (7 - i))},${128 +
+            128 / (i + 1)},${0})`,
+          "line-width": 4
+        },
+        filter: ["==", "trafficIntensity", `${i}`]
+      });
+      map.on("mouseenter", `traffic-${i}`, onMouseEnterHandler);
+      map.on("mouseleave", `traffic-${i}`, onMouseLeaveHandler);
+    }
+  });
 };
 
 function App() {
+  const [day, setDay] = useState(1);
+  const [timestamp, setTimestamp] = useState("20190501000053");
+
   useEffect(() => {
-    initializeMap();
-  }, []);
-  return ( < div className = "App" >
-    <
-    div id = "map" / >
-    <
-    /div>
+    initializeMap(day, timestamp);
+  }, [day, timestamp]);
+
+  return (
+    <div className="App">
+      <InputField
+        day={day}
+        setDay={setDay}
+        timestamp={timestamp}
+        setTimestamp={setTimestamp}
+      />
+      <div id="map" />
+    </div>
   );
 }
 
---
+++ src/InputField.js
@@ -0,0 +1,40 @@
+import React from "react";
+export function InputField({ day, setDay, timestamp, setTimestamp }) {
+  return (
+    <div className="input-field">
+      <form action="">
+        <label htmlFor="">
+          Date:
+          <input
+            type="date"
+            name="date"
+            value={`2019-05-${`${day}`.padStart(2, "0")}`}
+            min="2019-05-01"
+            max="2019-05-31"
+            onChange={e => setDay(parseInt(e.target.value.substring(8)))}
+          />
+        </label>
+        <label>
+          Time:
+          <input
+            type="time"
+            name="time"
+            step={60 * 5}
+            value={`${timestamp.substring(8, 10)}:${timestamp.substring(
+              10,
+              12
+            )}`}
+            onChange={e =>
+              setTimestamp(
+                `201905${`${day}`.padStart(2, "0")}${e.target.value.substring(
+                  0,
+                  2
+                )}${e.target.value.substring(2, 4)}`
+              )
+            }
+          />
+        </label>
+      </form>
+    </div>
+  );
+}
